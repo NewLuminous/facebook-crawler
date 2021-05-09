074f2be7f07e4100bb6d48c4b927ffd60f2f938f
NewLuminous <newluminous7@gmail.com>
2021-05-10 03:37:02 +0700

    Test one profile only

--- main_page.py
+++ main_page.py
@@ -35,5 +35,29 @@ for i in range(1):
 story_feedbacks = driver.find_elements_by_xpath('//div[@class="story_body_container"]/following-sibling::footer//a')
 
 # Go into a post
-story_url = story_feedbacks[2].get_attribute('href')
+story_url = story_feedbacks[0].get_attribute('href')
 driver.get(story_url)
+sleep(1)
+print('Successfully go into a post')
+
+# Go into reaction page
+reaction_url = driver.find_element_by_xpath('//div[@id="add_comment_switcher_placeholder"]/following-sibling::div//a').get_attribute('href')
+driver.get(reaction_url)
+sleep(1)
+print('Successfully go into reaction page')
+
+# Go into profile
+profile_url = driver.find_element_by_xpath('//div[@id="reaction_profile_browser"]//a').get_attribute('href')
+driver.get(profile_url)
+sleep(1)
+print('Successfully go into profile')
+
+# Go into details
+details_url = driver.find_element_by_xpath('//div[@id="profile_intro_card"]//a').get_attribute('href')
+driver.get(details_url)
+sleep(1)
+print('Successfully go into details')
+
+# About me
+about_me = driver.find_element_by_xpath('//div[@class="aboutme"]')
+print(about_me.get_attribute('innerHTML'))
